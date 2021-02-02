0c4cc2a432542dc8e8d2d0c18642aac4e58fdf51
David Wildasin <dragonmaus@posteo.net>
2021-02-01 21:14:33 -0500

    Add cvs stuff

---
+++ etc/update.d/cvs.sh
@@ -0,0 +1,8 @@
+echo '>> Updating CVS repositories'
+r=$(uname -r | tr . _)
+for repo in /usr/{src,xenocara,ports}
+do
+	[ -d $repo ] || continue
+	echo - $repo
+	(cd $repo && exec cvs -q update -Pd -r OPENBSD_$r)
+done
